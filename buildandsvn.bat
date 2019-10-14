d1cb4fa67c4f3d47d5e1778ab4fa56a9efc509cc
cmforjava <cmforjava@126.com>
2019-10-14 17:28:39 +0800

    记录一下bat

---
+++ buildandsvn.bat
@@ -0,0 +1,1 @@
+cd /d C:\svnprojects\kuaidi.askyiwu.cn\vuekuaidi\vue-admin-template&&npm run build:prod&cd /d C:\svnprojects\kuaidi.askyiwu.cn\public\stage&svn add . --no-ignore --force&start "" "."
