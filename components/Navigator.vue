138e26d62289096f2b5645799d6c383288981f9f
cmforjava <cmforjava@126.com>
2020-01-13 11:39:08 +0800

    最外层样式可以直接赋值,无须props

--- components/Navigator.vue
+++ components/Navigator.vue
@@ -1,64 +1,56 @@
 <template>
-    <div class="nav-outer mrow" :style="outerStyle">
-      <div class="left">
-        <slot name="left"></slot>
-      </div>
-      <div class="mid">
-        <slot name="mid">{{$route.meta.name}}</slot>
-      </div>
-      <div class="right">
-        <slot name="right"></slot>
-      </div>
+  <div class="nav-outer mrow">
+    <div class="left">
+      <slot name="left"></slot>
     </div>
+    <div class="mid">
+      <slot name="mid">{{$route.meta.name}}</slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Navigator",
-      data(){
-          return {
-
-          }
-      },
-      props: {
-          outerStyle: {
-            type: String,
-            default: ''
-          }
-      }
-    }
+export default {
+  name: "Navigator",
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  .nav-outer {
-    width:100vw;
-    height: 10.66vw;
+.nav-outer {
+  width: 100vw;
+  height: 10.66vw;
+}
+.left {
+  width: 10.66vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    width: 7.466vw;
+    height: 7.466vw;
   }
-  .left {
-    width: 10.66vw;
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-    img {
-      width: 7.466vw;
-      height: 7.466vw;
-    }
+}
+.mid {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 18px;
+}
+.right {
+  width: 10.66vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    width: 7.466vw;
+    height: 7.466vw;
   }
-  .mid {
-    flex: 1;
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-    font-size: 18px;
-  }
-  .right {
-    width: 10.66vw;
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-    img {
-      width: 7.466vw;
-      height: 7.466vw;
-    }
-  }
+}
 </style>
